e602a8e1ec179a802b14ee65d3ae084ffd04a858
Alwin_Joshy <flamencoresonance@gmail.com>
2020-02-04 23:19:01 +0400

    test commit

---
+++ Assets/A_Alwin/Scripts/CutExperimentClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutExperimentClass : MonoBehaviour
+{
+    Plane bladePlane;
+    void Start()
+    {
+        bladePlane = new Plane();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
