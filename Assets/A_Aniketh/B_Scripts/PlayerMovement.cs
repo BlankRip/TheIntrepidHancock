c6a2e89028870de31e67ee34a7f378c41bcd6eea
BlankRip <aniketh_g5@outlook.com>
2020-02-04 22:50:14 +0400

    Movement Scripts added and kinda decently working but need to fix rotation

---
+++ Assets/A_Aniketh/B_Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    Vector3 referanceVelocity = Vector3.zero;
+    Vector3 targetVelocity;
+    [SerializeField] Collider crouchDisableCollider;                     //The collider that will be disabled when the player is crouching
+    [Range(0,1)] [SerializeField] float crouchSpeedReduction;            //The amount by which the speed should be reduced while crouching
+    [Range(0, 0.5f)] [SerializeField] float smoothMovementBy;            //The time it takes to reach the target velocity
+    [Range(0, 0.5f)] [SerializeField] float moveThreshHold;              //The thresh hold values, the player will move when inputs is greater than this values
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();    //Getting rigid body of the gameObject
+    }
+
+    public void Movement(float horizontalMove, float verticalMove, float speed, bool crouch)
+    {
+        //Checking if Crouching, if so then disabeling the extra part of the collider
+        if(crouch)
+        {
+            speed = speed * crouchSpeedReduction;
+            if (crouchDisableCollider != null)
+                crouchDisableCollider.enabled = false;
+        }
+        else
+        {
+            if (crouchDisableCollider != null)
+                crouchDisableCollider.enabled = true;
+        }
+
+        //The velocity at which the object needs to be
+        targetVelocity = new Vector3(horizontalMove * speed, rb.velocity.y, verticalMove * speed);
+
+        //If the input values are over a perticualr thresh hold then the object will move with a desired velocity
+        if (horizontalMove > moveThreshHold || horizontalMove < -moveThreshHold || verticalMove > moveThreshHold || verticalMove < -moveThreshHold)
+        {
+            rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref referanceVelocity, smoothMovementBy);
+        }
+        
+        if (horizontalMove > 0 && horizontalMove < moveThreshHold)
+            transform.localRotation = Quaternion.EulerAngles(transform.localRotation.x, transform.localRotation.y + 90, transform.localRotation.z);
+        else if (horizontalMove < 0 && horizontalMove > -moveThreshHold)
+            rb.rotation = Quaternion.EulerAngles(transform.localRotation.x, transform.localRotation.y - 90, transform.localRotation.z);
+        else if (verticalMove > 0 && verticalMove < moveThreshHold)
+            rb.rotation = Quaternion.EulerAngles(transform.localRotation.x, transform.localRotation.y - 180, transform.localRotation.z);
+        else if (verticalMove < 0 && verticalMove > -moveThreshHold)
+            rb.rotation = Quaternion.EulerAngles(transform.localRotation.x, transform.localRotation.y + 180, transform.localRotation.z);
+    }
+}
