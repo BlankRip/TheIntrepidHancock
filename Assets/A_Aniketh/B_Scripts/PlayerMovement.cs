75ff66bf4e1f16b0296a7cec63d1033871eb731d
BlankRip <aniketh_g5@outlook.com>
2020-02-05 12:49:37 +0400

    make the player rotate with threshold but still chooppy

--- Assets/A_Aniketh/B_Scripts/PlayerMovement.cs
+++ Assets/A_Aniketh/B_Scripts/PlayerMovement.cs
@@ -7,8 +7,11 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody rb;
     Vector3 referanceVelocity = Vector3.zero;
     Vector3 targetVelocity;
+    Quaternion turnAngle;
     [SerializeField] Collider crouchDisableCollider;                     //The collider that will be disabled when the player is crouching
     [Range(0,1)] [SerializeField] float crouchSpeedReduction;            //The amount by which the speed should be reduced while crouching
+    [SerializeField] Transform rotateAround;                             //The object used as ref to rotate
+    [SerializeField] float rotationSpeed;                                //How fast will it rotate
     [Range(0, 0.5f)] [SerializeField] float smoothMovementBy;            //The time it takes to reach the target velocity
     [Range(0, 0.5f)] [SerializeField] float moveThreshHold;              //The thresh hold values, the player will move when inputs is greater than this values
 
@@ -40,14 +43,27 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref referanceVelocity, smoothMovementBy);
         }
-        
+
+        //If less than thresh hold speed then it will just rotate in that direction
         if (horizontalMove > 0 && horizontalMove < moveThreshHold)
-            transform.localRotation = Quaternion.EulerAngles(transform.localRotation.x, transform.localRotation.y + 90, transform.localRotation.z);
+        {
+            turnAngle = Quaternion.Euler(0, rotateAround.eulerAngles.y + 90, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, turnAngle, rotationSpeed * Time.deltaTime);
+        }
         else if (horizontalMove < 0 && horizontalMove > -moveThreshHold)
-            rb.rotation = Quaternion.EulerAngles(transform.localRotation.x, transform.localRotation.y - 90, transform.localRotation.z);
+        {
+            turnAngle = Quaternion.Euler(0, rotateAround.eulerAngles.y - 90, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, turnAngle, rotationSpeed * Time.deltaTime);
+        }
         else if (verticalMove > 0 && verticalMove < moveThreshHold)
-            rb.rotation = Quaternion.EulerAngles(transform.localRotation.x, transform.localRotation.y - 180, transform.localRotation.z);
+        {
+            turnAngle = Quaternion.Euler(0, rotateAround.eulerAngles.y, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, turnAngle, rotationSpeed * Time.deltaTime);
+        }
         else if (verticalMove < 0 && verticalMove > -moveThreshHold)
-            rb.rotation = Quaternion.EulerAngles(transform.localRotation.x, transform.localRotation.y + 180, transform.localRotation.z);
+        {
+            turnAngle = Quaternion.Euler(0, rotateAround.eulerAngles.y + 180, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, turnAngle, rotationSpeed * Time.deltaTime);
+        }
     }
 }
