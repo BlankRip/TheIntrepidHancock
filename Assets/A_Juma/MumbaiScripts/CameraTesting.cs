7b007f20bf4f409e14cb16cc3264acec444e9c49
turmoil2k <unconfigured@null.spigotmc.org>
2020-02-07 05:02:13 +0400

    Pushed wall collision camera still need to understand it a bit more, small difficulties left maybe 10-20% work left mostly done though.

--- Assets/A_Juma/MumbaiScripts/CameraTesting.cs
+++ Assets/A_Juma/MumbaiScripts/CameraTesting.cs
@@ -4,7 +4,60 @@ using UnityEngine;
 
 public class CameraTesting : MonoBehaviour
 {
-    // camera smooth
+    public Transform target, player;
+    public float mouseX;
+    public float mouseY;
+    float rotSpeed = 2;
+
+    public float verticalClampMin;
+    public float verticalClampMax;
+
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+    public float minDistance = 1.0f;
+    public float maxDistance = 4.0f;
+    public float smooth = 10.0f;
+    Vector3 dollyDir;
+    public Vector3 dollyDirAdjusted;
+    public float distance;
+
+    void Awake()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        //wall collision
+        dollyDir = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
+    }
+
+    void Update()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotSpeed;
+        mouseY = Mathf.Clamp(mouseY, verticalClampMin, verticalClampMax);
+
+        transform.LookAt(target);
+
+        target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        // 
+        Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir * maxDistance);
+        RaycastHit hit;
+        if (Physics.Linecast(transform.parent.position, desiredCameraPos, out hit))
+        {
+            distance = Mathf.Clamp((hit.distance * 0.9f), minDistance, maxDistance);
+        }
+        else
+        {
+            distance = maxDistance;
+        }
+        transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDir * distance, Time.deltaTime * smooth);
+    }
+}
+
+/*
+{
+    // camera smooth//sphere cast
     /*
     public Transform target;
     public float smoothTime = 0.3F;
@@ -66,35 +119,48 @@ public class CameraTesting : MonoBehaviour
         playerCam.LookAt(targetCenter);
     }*/
 
-    // camera rotation ============================================
-    public Transform target, player;
-    public float mouseX;
-    public float mouseY;
-    float rotSpeed = 2;
+    // camera rotation =========================================// ALL VALUES ARE PUBLIC BECAUSE OF TESTING REASONS IN THE EDITOR.
+    /*
+    public Transform target, player;                            // 
+    public float mouseX;                                        //
+    public float mouseY;                                        //
+    float rotSpeed = 2;                                         //
 
-    public float zoom;
-    public float zoomSpeed;
+    public float zoom;                                          // Base Zoom value
+    public float zoomSpeed;                                     
 
-    public float zoomMin;
-    public float zoomMax;
+    public float zoomMin;                                       // Minimum Zoom
+    public float zoomMax;                                       // Maximum Zoom
 
-    public float verticalClampMin;
-    public float verticalClampMax;
+    public float verticalClampMin;                              // Y AXIS Clamp MIN
+    public float verticalClampMax;                              // Y AXIS Clamp MAX
 
     //=========
-    public float smoothTime = 0.3F;
-    private Vector3 velocity = Vector3.zero;
+    public float smoothTime = 0.3F;                             //
+    private Vector3 velocity = Vector3.zero;                    //
 
-    void Start()
+    //==== wall collision
+    public float minDistance = 1.0f;
+    public float maxDistance = 4.0f;
+    public float smooth = 10.0f;
+    Vector3 dollyDir;
+    public Vector3 dollyDirAdjusted;
+    public float distance;
+
+    void Awake()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        //wall collision
+        dollyDir = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
     }
 
-    void Update()
-    {
-        zoom += Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
-
+   // void Update()
+    //{
+        /*
+        zoom += Input.GetAxis("Mouse ScrollWheel") * zoomSpeed; //zoom will increase or decrease depending on where the scroll is + / -
+        // === CLAMPING ZOOMS ===
         if (zoom > zoomMin)
         {
             zoom = zoomMin;
@@ -104,26 +170,41 @@ public class CameraTesting : MonoBehaviour
         {
             zoom = zoomMax;
         }
-
-        transform.localPosition = new Vector3(0, 0, zoom); // explain
-
+        // === CLAMPING ZOOMS ===
+        transform.localPosition = new Vector3(0, 0, zoom); // explain 
+        */
+        /*
         mouseX += Input.GetAxis("Mouse X") * rotSpeed;
         mouseY -= Input.GetAxis("Mouse Y") * rotSpeed;
-        mouseY = Mathf.Clamp(mouseY, verticalClampMin, verticalClampMax);
+        mouseY = Mathf.Clamp(mouseY, verticalClampMin, verticalClampMax); //Control mouseY Clamp between 2 values MIN & MAX
 
         transform.LookAt(target);
 
-        target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
-        //player.rotation = Quaternion.Euler(0, mouseX, 0); // enable this to let the player rotate with cam
-        //transform.position = Vector3.Lerp(transform.position, transform.position, Time.deltaTime);
-        //====================================
-        // camera smooth
-        // Define a target position above and behind the target transform
-        //Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+        target.rotation = Quaternion.Euler(mouseY, mouseX, 0); //storing quaternion rotations with mousey and mousex
+    */
+                                                               //player.rotation = Quaternion.Euler(0, mouseX, 0); // enable this to let the player rotate with cam
+                                                               //transform.position = Vector3.Lerp(transform.position, transform.position, Time.deltaTime);
+                                                               //====================================
+                                                               // camera smooth
+                                                               // Define a target position above and behind the target transform
+                                                               //Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
 
         // Smoothly move the camera towards that target position
         //transform.position = Vector3.SmoothDamp(transform.position, target.position, ref velocity, smoothTime);
 
-    }
-}
 
+        //========================wall collision
+        /*
+        Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir * maxDistance);
+        RaycastHit hit;
+        if (Physics.Linecast(transform.parent.position, desiredCameraPos, out hit))
+        {
+            distance = Mathf.Clamp((hit.distance * 0.9f), minDistance, maxDistance);
+        }
+        else
+        {
+            distance = maxDistance;
+        }
+        transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDir * distance, Time.deltaTime * smooth);
+    }
+}*/
