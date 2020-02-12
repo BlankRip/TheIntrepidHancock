125964e99f17eef70bba83d5c470834f0558a394
BlankRip <aniketh_g5@outlook.com>
2020-02-12 19:16:43 +0400

    Added code to test the reduce health by reducing health on colliding with the enemy

--- Assets/A_Aniketh/B_Scripts/Player.cs
+++ Assets/A_Aniketh/B_Scripts/Player.cs
@@ -65,30 +65,36 @@ public class Player : MonoBehaviour
 
 
 
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            myStats.ReduceHealth(20);
+        }
+    }
 
     //Maybe use as ref for animations
     //void RotationAnimation()
     //{
-        //If less than thresh hold speed then it will just rotate in that direction
-        //if (verticalMove > 0 && verticalMove < moveThreshHold)
-        //{
+    //If less than thresh hold speed then it will just rotate in that direction
+    //if (verticalMove > 0 && verticalMove < moveThreshHold)
+    //{
 
-        //}
-        //else if (verticalMove < 0 && verticalMove > -moveThreshHold)
-        //{
-        //    turnAngle = Quaternion.Euler(0, rotateAround.eulerAngles.y + 180, 0);
-        //    rb.rotation = Quaternion.Slerp(transform.rotation, turnAngle, rotationSpeed);
-        //}
-        //else if (horizontalMove > 0 && horizontalMove < moveThreshHold)
-        //{
-        //    turnAngle = Quaternion.Euler(0, rotateAround.eulerAngles.y + 90, 0);
-        //    rb.rotation = Quaternion.Slerp(transform.rotation, turnAngle, rotationSpeed);
-        //}
-        //else if (horizontalMove < 0 && horizontalMove > -moveThreshHold)
-        //{
-        //    turnAngle = Quaternion.Euler(0, rotateAround.eulerAngles.y - 90, 0);
-        //    rb.rotation = Quaternion.Slerp(transform.rotation, turnAngle, rotationSpeed);
-        //}
+    //}
+    //else if (verticalMove < 0 && verticalMove > -moveThreshHold)
+    //{
+    //    turnAngle = Quaternion.Euler(0, rotateAround.eulerAngles.y + 180, 0);
+    //    rb.rotation = Quaternion.Slerp(transform.rotation, turnAngle, rotationSpeed);
+    //}
+    //else if (horizontalMove > 0 && horizontalMove < moveThreshHold)
+    //{
+    //    turnAngle = Quaternion.Euler(0, rotateAround.eulerAngles.y + 90, 0);
+    //    rb.rotation = Quaternion.Slerp(transform.rotation, turnAngle, rotationSpeed);
+    //}
+    //else if (horizontalMove < 0 && horizontalMove > -moveThreshHold)
+    //{
+    //    turnAngle = Quaternion.Euler(0, rotateAround.eulerAngles.y - 90, 0);
+    //    rb.rotation = Quaternion.Slerp(transform.rotation, turnAngle, rotationSpeed);
+    //}
     //}
 }
