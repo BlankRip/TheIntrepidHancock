43130726b2df7fe753c0fc34d505c8b71c6b0fee
BlankRip <aniketh_g5@outlook.com>
2020-02-14 20:09:27 +0400

    Added GameOver Screen Script

---
+++ Assets/A_Aniketh/B_Scripts/GameOverScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+    void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);       //Loading the menu scene
+    }
+}
