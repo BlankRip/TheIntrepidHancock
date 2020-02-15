fa2235d7f06fab8095ede16321ecbb099f5a3ad4
turmoil2k <juma1112009@hotmail.com>
2020-02-15 12:02:06 +0400

    please ignore this commit its just for testing, going to change a lot of things so im going to go back on this if it goes worse.

---
+++ Assets/A_Juma/Phase 2 scripts/WeaponCheck.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCheck : MonoBehaviour
+{
+
+    public bool equipCheck;
+    public GameObject weaponType1;
+    public GameObject weaponType2;
+    public GameObject weaponType3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        equipCheck = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (equipCheck == true)
+        {
+            Debug.Log("Player currently has something equipped press G to drop OR press r to equip a new weapon and drop the currently held weapon");
+
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                weaponType1.GetComponent<Weapon>().DropWeapon();
+                //transform.DetachChildren();       r these 2 the same?         transform.SetParent(null);
+            }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+               // weaponType1.GetComponent<Weapon>().DropWeapon();
+            }
+        }
+    }
+}
