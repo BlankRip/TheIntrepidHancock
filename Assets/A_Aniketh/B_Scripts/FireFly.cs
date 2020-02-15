c94a582a48b71409c4b80377b9c5e393a2c68239
BlankRip <aniketh_g5@outlook.com>
2020-02-15 19:41:20 +0400

    The FireFly script added

---
+++ Assets/A_Aniketh/B_Scripts/FireFly.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFly : MonoBehaviour
+{
+    [SerializeField] string targetTag;           //The tag of the object the fierfly shouldFly to
+    Transform target;                            //The object that the firefly will go to
+    Vector3 startPosition;                       //The the position at which the firefly is spawned
+    Vector3 midPoint;                            //The third mid way point required to calculate the bezier curve
+    float distance;                              //The distance between the spawnPos and targetPos
+    float t;                                     //Tracker to perform the interpolation
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag(targetTag).transform;
+        startPosition = transform.position;
+        t = 0;
+    }
+
+    void Update()
+    {
+        distance = Vector3.Distance(startPosition, target.position);
+        //Getting mid point by getting direction to place the point and multiply with half the distance
+        midPoint = ((target.up.normalized + startPosition.normalized) / 2) * (distance / 2);
+        //Moving the firefly by interpolation wiht the bezier curve equation
+        transform.position = (1 - t) * (1 - t) * startPosition + (1 - t) * 2 * t * midPoint + t * t * target.position;
+        if (t <= 1)
+            t += Time.deltaTime;
+        else
+            Destroy(gameObject);     //Destroy the object when reached the target
+
+    }
+}
---
+++ Assets/A_Aniketh/B_Scripts/FlySpawnTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlySpawnTest : MonoBehaviour
+{
+    [SerializeField] GameObject fireFly;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            Instantiate(fireFly, transform.position, Quaternion.identity);
+    }
+}
