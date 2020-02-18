bf48d590bb1a5454df2474a884fdb6e13ca9bd12
BlankRip <aniketh_g5@outlook.com>
2020-02-18 13:16:42 +0400

    Breaking objects, force to broken pieces and hits, but no effects for hits, need to do that

---
+++ Assets/A_Aniketh/B_Scripts/BreakPieceExplosion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakPieceExplosion : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float explosionForce = 7;
+    [Range(0.0f, 0.5f)] [SerializeField] float explosionRaious = 0.01f;
+    [SerializeField] float upWardsModifier = 0.00001f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddExplosionForce(explosionForce, transform.position, explosionRaious, upWardsModifier, ForceMode.Impulse);
+    }
+}
---
+++ Assets/A_Aniketh/B_Scripts/BreakableObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableObject : MonoBehaviour
+{
+    [SerializeField] GameObject fractureVersion;
+    [SerializeField] GameObject relic;
+    [SerializeField] int scoreToAdd;
+    [SerializeField] int hitsBeforeBreak = 2;
+    int hitsTaken;
+
+    void Start()
+    {
+        hitsTaken = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            if(hitsTaken <= hitsBeforeBreak)
+                hitsTaken++;
+                //Debug.Log("<color=pink>Hit</color>" + hitsTaken + "<color=pink>BeforeBreak</color>" + hitsBeforeBreak);
+            if (hitsTaken > hitsBeforeBreak)
+            {
+                Instantiate(fractureVersion, transform.position, transform.rotation);
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+}
