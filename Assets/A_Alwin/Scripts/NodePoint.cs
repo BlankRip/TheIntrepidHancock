02ced492235dcced831e7d92ff0f7af5d4843988
AlwinJoshy <flamencoresonance@gmail.com>
2020-03-05 00:58:14 +0400

    A* Star Pathfinding almost done

--- Assets/A_Alwin/Scripts/NodePoint.cs
+++ Assets/A_Alwin/Scripts/NodePoint.cs
@@ -7,7 +7,7 @@ public class NodePoint : MonoBehaviour
 {
     public static GameObject[] allNodes;
     public Node thisNode;
-
+    public bool drawGizmo;
     // go through all the nodes as
     public void FindFriends()
     {
@@ -31,11 +31,11 @@ public class NodePoint : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        if (thisNode.neighbours.Length > 0)
+        if (drawGizmo && thisNode.neighbours.Length > 0)
         {
             foreach (Node item in thisNode.neighbours)
             {
-                Gizmos.color = Color.yellow;
+                Gizmos.color = Color.gray;
                 Gizmos.DrawLine(transform.position, item.position);
             }
         }
