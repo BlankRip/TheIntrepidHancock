17d06c3274d72767c8729059e535ce408f4bf2fb
BlankRip <aniketh_g5@outlook.com>
2020-03-25 12:32:41 +0400

    Cool down Leaf Node done

---
+++ Assets/A_Aniketh/B_Scripts/TheGame/AI/Leaf/Chase & Attack/CoolDownNode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolDownNode : TreeNode
+{
+    float coolDownTimer = 3;
+    float coolDownTracker = 3;
+
+    public override void Run(TheAI ai)
+    {
+        if(coolDownTracker <= 0)
+        {
+            coolDownTracker = coolDownTimer;
+            status = ReturnResult.Success;
+            return;
+        }
+
+        coolDownTracker -= Time.deltaTime * ai.coolDownSpeed;
+        status = ReturnResult.Running;
+    }
+}
--- Assets/A_Aniketh/B_Scripts/TheGame/AI/TheAI.cs
+++ Assets/A_Aniketh/B_Scripts/TheGame/AI/TheAI.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class TheAI : MonoBehaviour
 {
     public bool playerFound;
+    public Animator myAnimator;
+    [Range(0, 1)] public float coolDownSpeed = 1;
 
     TreeNode root;
 
