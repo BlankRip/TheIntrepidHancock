56d04afa93740a4877f44b4fba262d3e4ffad786
BlankRip <aniketh_g5@outlook.com>
2020-03-25 14:01:53 +0400

    Attack Node

---
+++ Assets/A_Aniketh/B_Scripts/TheGame/AI/Leaf/Chase & Attack/AttackNode.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackNode : TreeNode
+{
+    float clipLength;
+
+    public override void Run(TheAI ai)
+    {
+        if (status != ReturnResult.Running)
+        {
+            Debug.Log("<color=red>Enemy attack anim triggered</color>");
+            Debug.Log("<color=red>Enemy attack sound</color>");
+            clipLength = ai.myAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.length;
+        }
+
+        if(clipLength <=0)
+        {
+            status = ReturnResult.Success;
+            return;
+        }
+
+        clipLength -= Time.deltaTime;
+        status = ReturnResult.Running;
+    }
+}
