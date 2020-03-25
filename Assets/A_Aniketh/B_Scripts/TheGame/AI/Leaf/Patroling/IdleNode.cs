8781786692de0132d64d68fc425b0b4c7c271b60
BlankRip <aniketh_g5@outlook.com>
2020-03-25 13:11:06 +0400

    Updated Idle Node

---
+++ Assets/A_Aniketh/B_Scripts/TheGame/AI/Leaf/Patroling/IdleNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleNode : TreeNode
+{
+    float idleEndTime = 7;
+    float idleTimeTracker = 7;
+
+    public override void Run(TheAI ai)
+    {
+        if(idleTimeTracker <= 0)
+        {
+            Debug.Log("Enemy Idleing anim bool to false");
+            idleTimeTracker = idleEndTime;
+            status = ReturnResult.Success;
+            return;
+        }
+
+        Debug.Log("Enemy Idleing anim bool to true");
+        idleTimeTracker -= Time.deltaTime;
+        status = ReturnResult.Running;
+    }
+}
--- Assets/A_Aniketh/B_Scripts/TheGame/AI/TheAI.cs
+++ Assets/A_Aniketh/B_Scripts/TheGame/AI/TheAI.cs
@@ -7,6 +7,7 @@ public class TheAI : MonoBehaviour
     public bool playerFound;
     public Animator myAnimator;
     [Range(0, 1)] public float coolDownSpeed = 1;
+    [Range(1, 2)] public float idleTimeSpeed = 1;
 
     TreeNode root;
 
