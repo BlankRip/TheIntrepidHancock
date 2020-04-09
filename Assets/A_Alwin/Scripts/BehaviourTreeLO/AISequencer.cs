8b4cac97eb8d53a2ce516b1714752abe1d88be39
AlwinJoshy <flamencoresonance@gmail.com>
2020-04-09 13:10:04 +0400

    any minor changes i don't remember

---
+++ Assets/A_Alwin/Scripts/BehaviourTreeLO/AISequencer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlwinScript.AI
+{
+    public class AISequencer : AINode
+    {
+        public override AIState Run(AIBlackboard ai)
+        {
+            foreach (AINode node in childNodes)
+            {
+                AIState state = node.Run(ai);
+                if (state == AIState.Fail || state == AIState.Running)
+                {
+                    return state;
+                }
+            }
+            return AIState.Success;
+        }
+    }
+}
