0bae49bf076bcd35dd293eb7a0d293e0ee2969d3
BlankRip <aniketh_g5@outlook.com>
2020-04-27 23:00:50 +0400

    Player new textures, Relic E pickup, weapon enemy interaction

--- Assets/Scripts/TheGame/RelicScript.cs
+++ Assets/Scripts/TheGame/RelicScript.cs
@@ -4,18 +4,54 @@ using UnityEngine;
 
 public class RelicScript : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    [SerializeField] Transform ePos;
+    GameObject pickUpUI;
+    bool canPickup;
+
+    private void Start()
     {
-        //If player collides with the relic then he collects it and the relics collected is increased by one on the tracker script
-        if(other.tag == "Player")
+        pickUpUI = GameManager.instance.eUIobj;
+    }
+
+    private void Update()
+    {
+        if(canPickup && Input.GetKeyDown(KeyCode.E))
         {
             Debug.Log("<color=blue> Relic Collected</color>");
+            pickUpUI.SetActive(false);
             GameManager.instance.SpawnEnemy();
             GameManager.instance.ReadyToExit();
             GameManager.instance.RelicUiUpdate();
             AudioManger.instance.RelicCollectedClip();
             Destroy(gameObject);
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //If player collides with the relic then he collects it and the relics collected is increased by one on the tracker script
+        if(other.tag == "Player")
+        {
+            pickUpUI.transform.position = ePos.position;
+            GameManager.instance.eResetUI = true;
+            pickUpUI.SetActive(true);
+            canPickup = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            pickUpUI.SetActive(true);
+            canPickup = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canPickup = false;
+        pickUpUI.SetActive(false);
     }
 
 }
--- Assets/Scripts/TheGame/Weapon/EquipManager.cs
+++ Assets/Scripts/TheGame/Weapon/EquipManager.cs
@@ -6,18 +6,17 @@ public class EquipManager : MonoBehaviour
 {
     [Tooltip("The position the weapon will equip")] 
     [SerializeField] Transform equipPosition;
-    [Tooltip("The UI text object")]
-    [SerializeField] GameObject pickUpUI;
     [Tooltip("How much above the weapon will the UI appere")]
     [SerializeField] float UIHight = 0.5f;
     Player objectToAttachTo;                                        //Status if the player has a weapon equipped
     J_Weapon currentWeapon;                                //The weapon which is corrently equipped
     Rigidbody currentWeaponRb;                               //The rigidbody component attached to the currently equipped weapon
-    public bool resetUI;
+    GameObject pickUpUI;                                    //The UI text object
 
     private void Start()
     {
         objectToAttachTo = FindObjectOfType<Player>();
+        pickUpUI = GameManager.instance.eUIobj;
     }
 
     public void EquipWeapon(J_Weapon weapon, Rigidbody rb)
@@ -41,7 +40,7 @@ public class EquipManager : MonoBehaviour
     public void DropWeapon(J_Weapon weapon, Rigidbody rb)
     {
         //Dropping the weapon
-        resetUI = true;
+        GameManager.instance.eResetUI = true;
         weapon.transform.SetParent(null);
         rb.isKinematic = false;
         rb.AddForce(transform.forward * 10f, ForceMode.Impulse);
@@ -53,7 +52,7 @@ public class EquipManager : MonoBehaviour
     public void ShowUI(J_Weapon weapon)
     {
         pickUpUI.transform.position = new Vector3(weapon.transform.position.x, weapon.transform.position.y + UIHight, weapon.transform.position.z);
-        resetUI = true;
+        GameManager.instance.eResetUI = true;
         pickUpUI.SetActive(true);
     }
 
