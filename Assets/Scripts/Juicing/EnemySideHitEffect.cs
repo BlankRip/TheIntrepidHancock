91f13d9d2a5efbb1ad4fa9fa26125ff8339bccab
AlwinJoshy <flamencoresonance@gmail.com>
2020-04-30 02:05:36 +0400

    New Light Baking, New Orange Blue

---
+++ Assets/Scripts/Juicing/EnemySideHitEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySideHitEffect : MonoBehaviour
+{
+    public ParticleSystem slapEffect;
+
+
+    public void OnHitAction()
+    {
+        slapEffect.Play();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OnHitAction();
+        }
+    }
+
+}
